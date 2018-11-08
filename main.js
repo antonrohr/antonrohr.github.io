9c3ae760a4f79070fb64c6b8ffc57503995e8460
Anton Rohr <anton.rohr@gmx.de>
2018-11-08 19:13:54 +0100

    fixed main.js

--- main.js
+++ main.js
@@ -1,43 +1,42 @@
 function get(url) {
 	// Return a new promise.
 	return new Promise(function(resolve, reject) {
-	  // Do the usual XHR stuff
-	  var req = new XMLHttpRequest();
-	  req.open('GET', url);
-  
-	  req.onload = function() {
+		// Do the usual XHR stuff
+		var req = new XMLHttpRequest();
+		req.open('GET', url);
+	
+		req.onload = function() {
 		// This is called even on 404 etc
 		// so check the status
 		if (req.status == 200) {
-		  // Resolve the promise with the response text
-		  resolve(req.response);
+			// Resolve the promise with the response text
+			resolve(req.response);
 		}
 		else {
-		  // Otherwise reject with the status text
-		  // which will hopefully be a meaningful error
-		  reject(Error(req.statusText));
+			// Otherwise reject with the status text
+			// which will hopefully be a meaningful error
+			reject(Error(req.statusText));
 		}
-	  };
-  
-	  // Handle network errors
-	  req.onerror = function() {
+		};
+	
+		// Handle network errors
+		req.onerror = function() {
 		reject(Error("Network Error"));
-	  };
+		};
 
-	  // Make the request
-	  req.send();
+		// Make the request
+		req.send();
 	});
 }
 
 
 
-
-
-// use : 
-get('README.md').then(function(response) {
-	console.log(response)
-	var converter = new showdown.Converter();
-	document.body = converter.makeHTML(response)
-}, function(error) {
-	console.error("Failed!", error);
+document.addEventListener("DOMContentLoaded", function() {
+	get('README.md').then(function(response) {
+		console.log(response)
+		var converter = new showdown.Converter();
+		document.body = converter.makeHTML(response)
+	}, function(error) {
+		console.error("Failed!", error);
+	});
 });
